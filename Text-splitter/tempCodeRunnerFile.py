3fc26e58ccf18f1a44a61d49affc83c6660b7ac6
himanshu-shekhar2327 <himanshu.shekhar0505@gmail.com>
2026-07-02 13:29:18 +0530

    Document Loader and Text Splitter in Langchain

---
+++ Text-splitter/tempCodeRunnerFile.py
@@ -0,0 +1,33 @@
+from langchain_text_splitters import CharacterTextSplitter
+# from langchain_community.document_loaders import PyPDFLoader
+
+# text = """Artificial Intelligence is transforming industries.
+# Machine Learning is a subset of AI.
+# Deep Learning is a subset of Machine Learning.
+# Transformers are neural networks.
+# Large Language Models use transformers.
+# """
+
+# splitter = CharacterTextSplitter(
+#     chunk_size=100,
+#     chunk_overlap=0,
+#     separator=""
+# )
+
+# result = splitter.split_text(text)
+
+# print(result)
+
+
+# splitter = CharacterTextSplitter(
+#     chunk_size =30,
+#     chunk_overlap=0,
+#     separator=''
+# )
+
+# result = splitter.split_text(text)
+
+# print(result[0])
+# print()
+# print(result)
+# print(result[0].page_content())
